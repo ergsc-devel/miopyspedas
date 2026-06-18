d64fc1292069f9c4f0be60cd20b3c1c974db708f
Kazuhiro Yamamoto <yamamoto.kazuhiro@isee.nagoya-u.ac.jp>
2026-06-18 15:09:01 +0900

    add the MGF directory

---
+++ miopyspedas/bc/mmo/mgf/mgf.py
@@ -0,0 +1,52 @@
+from ..load import load
+from pytplot import options
+
+import logging
+
+def mgf(
+    trange=["2021-8-10","2021-8-11"],
+    level="l2pre",
+    prefix="",
+    suffix="",
+    get_support_data=False,
+    varformat=None,
+    varnames=[],
+    downloadonly=False,
+    notplot=False,
+    no_update=False,
+    time_clip=True,
+    force_download=False,
+    uname=None, passwd=None,
+):
+
+    """
+    This function loads data from the MGF experiment from Mercury Magnetospheric Orbiter (MMO) of the Bepicolombo mission
+
+    Parameters
+    ----------
+
+    """
+    
+    if prefix is None:
+        prefix = ""
+
+    if suffix is None:
+        suffix = ""
+
+    mgf_vars = load(trange=trange,
+                    instrument='mgf',
+                    level=level,
+                    prefix=prefix,
+                    suffix=suffix,
+                    get_support_data=get_support_data,
+                    varformat=varformat,
+                    varnames=varnames,
+                    downloadonly=downloadonly,
+                    notplot=notplot,
+                    no_update=no_update,
+                    time_clip=time_clip,
+                    force_download=False,
+                    uname=None, passwd=None
+                    )
+
+    return mgf_vars
