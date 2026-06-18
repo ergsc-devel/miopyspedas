b0b995778016fe179fd43505d51644e9ad274051
Kazuhiro Yamamoto <yamamoto.kazuhiro@isee.nagoya-u.ac.jp>
2026-06-18 19:11:33 +0900

    {} -> {obs_mode}

--- miopyspedas/bc/mmo/mppe/ena/ena.py
+++ miopyspedas/bc/mmo/mppe/ena/ena.py
@@ -124,7 +124,7 @@ def ena(
     
     
     if files is None:
-        pathformat = f"satellite/mmo/cdf/mppe/ena/{level}/{data_mode}/{datatypeobs_mode}/{}/%Y/%m/" + \
+        pathformat = f"satellite/mmo/cdf/mppe/ena/{level}/{data_mode}/{datatype}/{obs_mode}/%Y/%m/" + \
             f"bc_mmo_mppe-ena_{lev}_{data_mode}-{obs_mode}_%Y%m%d_r??-v??-??.cdf"
     
     tvars = load(trange=trange,
